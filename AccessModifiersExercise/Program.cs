8f4989ba1f4e87c361330c658e555063bc28438d
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-10 12:31:54 +0200

    Add project files.

---
+++ AccessModifiersExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2205240#content
+namespace AccessModifiersExercise
+{
+    public class Person
+    {
+        private DateTime _birthdate; // setting it requires creating a method
+        //fields are supposed to be an implementation detail hidden from the outside world
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
